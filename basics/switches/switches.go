2582dea4467f552d8ecbbc1b3b0e789080459bfc
Louis Serianni <124912943+serlouis@users.noreply.github.com>
2025-10-29 17:32:14 -0400

    added fizzbuzz for the heck of it

--- basics/switches/switches.go
+++ basics/switches/switches.go
@@ -55,5 +55,16 @@ func main() {
 	}
 	*/
 
-	
+	for i := 1; i <= 100; i++ {
+		switch {
+		case i % 5 == 0 && i % 3 == 0:
+			fmt.Println("fizzbuzz")
+		case i % 5 == 0:
+			fmt.Println("buzz")
+		case i % 3 == 0:
+			fmt.Println("fizz")
+		default:
+			fmt.Println(i)
+		}
+	}
 }
