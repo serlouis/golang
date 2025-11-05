55eb93565dafa8b9c494f37ad7481eeb91fdc7cd
Louis Serianni <124912943+serlouis@users.noreply.github.com>
2025-11-05 00:41:44 -0500

    defer.go

---
+++ basics/defer/defer.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	process(10)
+
+}
+
+func process(i int) {
+	defer fmt.Println("Deferred i value: ", i) // this will print 10 because this defer is encountered before we increment i
+	defer fmt.Println("First Deferred statement executed") // this will be deferred until the end of the function they are processed in LIFO order
+	defer fmt.Println("Second deferred statement excecuted")
+	defer fmt.Println("Third deferred statement executed")
+	i++
+	fmt.Println("Normal execution statement")
+	fmt.Println("Value of i: ", i)
+}
